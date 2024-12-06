be4be6ec44038444836e1b13f4be26ece362e245
1tneuq <quentlaco12@gmail.com>
2024-12-06 02:11:01 +0100

    automatisation récupération des abi et adresses de contrats

--- app.js
+++ app.js
@@ -6,231 +6,50 @@ if (typeof window.ethereum !== 'undefined') {
 
 //instance supply chain
 const web3 = new Web3(window.ethereum);
-const supplyChainAddress = '0xacF189b26836357e733b30A343B216dc60147FF9'; // Adresse du contratnpm start supply chain a changer apres migrate
-const supplyChainabi = [ // tableau à changer après chaque migrate
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "id",
-                "type": "uint256"
-            },
-            {
-                "indexed": false,
-                "internalType": "string",
-                "name": "name",
-                "type": "string"
-            },
-            {
-                "indexed": false,
-                "internalType": "enum SupplyChain.State",
-                "name": "state",
-                "type": "uint8"
-            }
-        ],
-        "name": "ProductUpdated",
-        "type": "event"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "productCount",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "products",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "id",
-                "type": "uint256"
-            },
-            {
-                "internalType": "string",
-                "name": "name",
-                "type": "string"
-            },
-            {
-                "internalType": "enum SupplyChain.State",
-                "name": "state",
-                "type": "uint8"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_name",
-                "type": "string"
-            }
-        ],
-        "name": "createProduct",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "_productId",
-                "type": "uint256"
-            },
-            {
-                "internalType": "enum SupplyChain.State",
-                "name": "_state",
-                "type": "uint8"
-            }
-        ],
-        "name": "updateState",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "_productId",
-                "type": "uint256"
-            }
-        ],
-        "name": "getProduct",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            },
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            },
-            {
-                "internalType": "enum SupplyChain.State",
-                "name": "",
-                "type": "uint8"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-//instance authentification
-const authAddress = '0x2aC1a9E1638713A89bd65a426DC07C763A675Ff7'; // Adresse de Auth.sol
-const authAbi = [ // tableau à changer après chaque migrate
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "userAddress",
-                "type": "address"
-            }
-        ],
-        "name": "UserAuthenticated",
-        "type": "event"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "userAddress",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "string",
-                "name": "username",
-                "type": "string"
-            }
-        ],
-        "name": "UserRegistered",
-        "type": "event"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_username",
-                "type": "string"
-            }
-        ],
-        "name": "register",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "authenticate",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-const auth = new web3.eth.Contract(authAbi, authAddress);
 
-const supplyChain = new web3.eth.Contract(supplyChainabi, supplyChainAddress);
-
-async function createProductWithAuth() {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-
+// chargement de l'ABI et l'adresse d'un contrat à partir des fichiers json buildés
+async function loadContractData(contractName) {
     try {
-        const username = await auth.methods.authenticate().call({ from: accounts[0] });
-        console.log(`Utilisateur authentifié : ${username}`);
-
-        const name = document.getElementById('productName').value;
-        await supplyChain.methods.createProduct(name).send({ from: accounts[0] });
-        alert('Produit créé avec succès.');
+        const response = await fetch(`./build/contracts/${contractName}.json`);
+        const contractData = await response.json();
+        return {
+            abi: contractData.abi,
+            address: contractData.networks[Object.keys(contractData.networks)[0]].address
+        };
     } catch (error) {
-        console.error('Erreur d\'authentification:', error);
-        alert('Vous devez être enregistré pour créer un produit.');
+        console.error(`Erreur lors du chargement des données du contrat ${contractName}:`, error);
+        throw error;
     }
 }
+
+let auth, supplyChain;
+
+//init des contrats auth et SupplyChain
+async function initializeContracts() {
+    try {
+        const authContractData = await loadContractData('Auth');
+        auth = new web3.eth.Contract(authContractData.abi, authContractData.address);
+        console.log(`Contrat Auth chargé : Adresse ${authContractData.address}`);
+
+        const supplyChainContractData = await loadContractData('SupplyChain');
+        supplyChain = new web3.eth.Contract(supplyChainContractData.abi, supplyChainContractData.address);
+        console.log(`Contrat SupplyChain chargé : Adresse ${supplyChainContractData.address}`);
+        
+    } catch (error) {
+        console.error('Erreur lors de l\'initialisation des contrats:', error);
+    }
+}
+
+// fct qui se declenche au chargement de la page
+window.onload = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+        console.log('MetaMask détecté');
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        await initializeContracts();
+    } else {
+        console.log('MetaMask non détecté');
+    }
+};
 
 
 async function updateProductStateWithAuth() {
